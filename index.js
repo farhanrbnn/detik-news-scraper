86c8478c0649dd373a24a95843d5f566b432b734
Farhan Rabbaanii <farhanrbnn@gmail.com>
2020-10-18 17:46:27 +0700

    make the object

--- index.js
+++ index.js
@@ -21,8 +21,47 @@ const URL = 'https://www.detik.com/terpopuler/news?utm_source=detiknews&utm_medi
 
 		}).get();
 
-		console.log(newsTitle);
-		
+		const data = await Promise.all(newsLink.map(async (link)=>{
+			try{
+				const res = await superagent.get(link);
+				const $ = cheerio.load(res.text);
+
+				const newsAuthor = $('.detail__author').map((i, section)=>{
+					const author = $(section).text();
+					return author;
+
+				}).get();
+
+				const releaseDate = $('.detail__date').map((i, section)=>{
+					return $(section).text();
+
+				}).get();
+
+				return {
+					newsAuthor,
+					releaseDate
+				}
+
+			} catch(err) {
+				console.log(err);
+			}
+		}));
+
+		data.forEach((val, index)=>{
+			let object = new Object();
+
+			const author = val.newsAuthor;
+			const date = val.releaseDate;
+
+			object.newsTitle = newsTitle[index];
+			object.author = author[0];
+			object.newsLink = newsLink[index];
+
+			console.log(object)
+			
+
+		})
+
 	} catch (err) {
 		console.log(err);
 
